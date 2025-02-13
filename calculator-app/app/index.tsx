bd5344993daea583df6ce9a5d0d46592d270b04f
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2025-02-13 16:08:19 +0700

    Refactor index.tsx to enhance layout and styling; add buttons and status bar for improved UI

--- calculator-app/app/index.tsx
+++ calculator-app/app/index.tsx
@@ -1,15 +1,67 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, StatusBar, SafeAreaView ,TouchableOpacity, Dimensions} from "react-native";
 
 export default function Index() {
+  
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView>
+        <Text style={styles.value}>0</Text>
+        <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity  style={styles.button}>
+      <Text style={styles.text}>text test</Text>
+    </TouchableOpacity>
+
+        </View>
+      </SafeAreaView>
     </View>
   );
+
+  
 }
+const screen = Dimensions.get("window");
+const buttonWidth = screen.width / 4;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#202020",
+    justifyContent: "flex-end"
+  },
+  value: {
+    color: "#fff",
+    fontSize: 40,
+    textAlign: "right",
+    marginRight: 20,
+    marginBottom: 10
+  },
+  button: {
+    backgroundColor: "#333333",
+    flex: 1,
+    height: Math.floor(buttonWidth - 10),
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: Math.floor(buttonWidth),
+    margin: 5
+  },
+  buttonDouble: {
+    width: screen.width / 2 - 10,
+    flex: 0,
+    alignItems: "flex-start",
+    paddingLeft: 40
+  },
+  buttonSecondary: {
+    backgroundColor: "#a6a6a6"
+  },
+  buttonAccent: {
+    backgroundColor: "#f09a36"
+  },
+  text: {
+    color: "#fff",
+    fontSize: 25
+  },
+  textSecondary: {
+    color: "#060606"
+  }
+});
